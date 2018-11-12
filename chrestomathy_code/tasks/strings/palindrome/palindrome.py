54fa7ae81356699b811f20a58d89b68154e3b764
aecant <aecantini@gmail.com>
2018-11-12 22:39:16 +0100

    better palindrome.py

--- chrestomathy_code/tasks/strings/palindrome/palindrome.py
+++ chrestomathy_code/tasks/strings/palindrome/palindrome.py
@@ -1,9 +1,9 @@
 import sys
 
 
-def is_palindrome(s):
-    low_alpha = list(map(str.casefold, filter(str.isalpha, s)))
-    return low_alpha == low_alpha[::-1]
+def is_palindrome(string):
+    lower_alpha = [char.casefold() for char in string if char.isalpha()]
+    return lower_alpha == lower_alpha[::-1]
 
 
 print('_true_' if is_palindrome(sys.argv[1]) else '_false_')
