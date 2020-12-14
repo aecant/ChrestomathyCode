5846fd3d1545c29513d35de7fd55967bed031b9b
Cantini Alessandro <aecantini@gmail.com>
2020-12-15 00:14:04 +0100

    Add trapping rain water

---
+++ chrestomathy_code/tasks/leetcode/trapping_rain_water/trapping_rain_water.py
@@ -0,0 +1,36 @@
+import sys
+from typing import List
+
+
+def count_half(height: List[int]) -> int:
+    sorted_tuples = sorted(enumerate(height), key=lambda t: t[1])
+    max_pos = 0
+    trapped = 0
+
+    while sorted_tuples:
+        next_max_pos, next_max_height = sorted_tuples.pop()
+
+        if next_max_pos <= max_pos:
+            continue
+
+        for h in height[max_pos + 1: next_max_pos]:
+            trapped += next_max_height - h
+
+        max_pos = next_max_pos
+
+    return trapped
+
+
+def count_trap_water(height: List[int]) -> int:
+    if not height:
+        return 0
+
+    max_pos, _ = max(enumerate(height), key=lambda t: t[1])
+
+    left_half = height[max_pos::-1]
+    right_half = height[max_pos:]
+
+    return count_half(left_half) + count_half(right_half)
+
+
+print(count_trap_water(list(map(int, sys.argv[1:]))))
